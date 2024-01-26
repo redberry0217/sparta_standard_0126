0bf155ffa331967f1cffe513618acaa4e2f8eb95
redberry0217 <redberry0217@gmail.com>
2024-01-26 18:05:18 +0900

    0126 박지영

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,18 @@ function App() {
   ];
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter(students => students.age >= 18);
+  console.log(filteredStudents)
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
+        {filteredStudents.map((students, idx) => {
+          <li key={idx}>
+            {students.name}
+            </li>
+        })}
         {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
       </ul>
